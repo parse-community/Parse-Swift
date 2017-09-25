9a17f336f1c094101dd779ebbcf87b07872b93df
Florent Vilmart <florent@flovilmart.com>
2017-09-25 14:36:59 -0400

    Keychain storage (#7)
    * Keychain storage
    
    * Better gating for availability

---
+++ TestHost/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  TestHost
+//
+//  Created by Florent Vilmart on 17-09-25.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
+class ViewController: UIViewController {}
--- Tests/ParseSwiftTests/ParseSwiftTests.swift
+++ Tests/ParseSwiftTests/ParseSwiftTests.swift
@@ -9,24 +9,4 @@
 import XCTest
 @testable import ParseSwift
 
-class ParseTests: XCTestCase {
-    override func setUp() {
-        super.setUp()
-    }
-
-    override func tearDown() {
-        super.tearDown()
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-}
+class ParseTests: XCTestCase {}
