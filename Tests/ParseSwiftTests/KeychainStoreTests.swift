600fa81c3cc4be77d88dc51b71c10bb918378edd
Shawn Baek <shawn@shawnbaek.com>
2018-12-30 23:54:19 +0900

    Fix Test Cases

--- Tests/ParseSwiftTests/KeychainStoreTests.swift
+++ Tests/ParseSwiftTests/KeychainStoreTests.swift
@@ -73,7 +73,7 @@ class KeychainStoreTests: XCTestCase {
     func testSetComplextObject() {
         let complexObject: [Any] = [["key": "value"], "string2", 1234, NSNull()]
         testStore["complexObject"] = complexObject
-        guard let retrievedObject: [Any] = testStore?["complexObject"] else {
+        guard let retrievedObject: [Any] = testStore["complexObject"] else {
             return XCTFail("Should retrieve the object")
         }
 
@@ -111,7 +111,8 @@ class KeychainStoreTests: XCTestCase {
 
     func testRemoveObject() {
         testStore["key1"] = "value1"
-        XCTAssertNotNil(testStore["key1"]!, "The value should be set")
+        let key1: String = testStore["key1"] ?? ""
+        XCTAssertNotNil(key1, "The value should be set")
         _ = testStore.removeObject(forKey: "key1")
         let key1Val: String? = testStore["key1"]
         XCTAssertNil(key1Val, "There should be no value after removal")
@@ -119,7 +120,8 @@ class KeychainStoreTests: XCTestCase {
 
     func testRemoveObjectSubscript() {
         testStore["key1"] = "value1"
-        XCTAssertNotNil(testStore["key1"]!, "The value should be set")
+        let key1: String = testStore["key1"] ?? ""
+        XCTAssertNotNil(key1, "The value should be set")
         testStore[string: "key1"] = nil
         let key1Val: String? = testStore["key1"]
         XCTAssertNil(key1Val, "There should be no value after removal")
@@ -128,8 +130,10 @@ class KeychainStoreTests: XCTestCase {
     func testRemoveAllObjects() {
         testStore["key1"] = "value1"
         testStore["key2"] = "value2"
-        XCTAssertNotNil(testStore["key1"]!, "The value should be set")
-        XCTAssertNotNil(testStore["key2"]!, "The value should be set")
+        let key1: String = testStore["key1"] ?? ""
+        let key2: String = testStore["key2"] ?? ""
+        XCTAssertNotNil(key1, "The value should be set")
+        XCTAssertNotNil(key2, "The value should be set")
         _ = testStore.removeAllObjects()
         let key1Val: String? = testStore["key1"]
         let key2Val: String? = testStore["key1"]
