3b745d189fc777899dfdc6a7a460f5573d2b3c5c
Corey's iMac <coreyearleon@icloud.com>
2020-07-12 16:54:59 -0400

    update playground var names

--- ParseSwift.playground/Pages/1- Your first Object.xcplaygroundpage/Contents.swift
+++ ParseSwift.playground/Pages/1- Your first Object.xcplaygroundpage/Contents.swift
@@ -28,18 +28,18 @@ struct GameScore: ParseSwift.ObjectType {
 
 var score = GameScore(score: 10)
 
-guard let score = try? score.save() else { fatalError() }
-assert(score.objectId != nil)
-assert(score.createdAt != nil)
-assert(score.updatedAt != nil)
-assert(score.score == 10)
+guard let savedScore = try? score.save() else { fatalError() }
+assert(savedScore.objectId != nil)
+assert(savedScore.createdAt != nil)
+assert(savedScore.updatedAt != nil)
+assert(savedScore.score == 10)
 
 // Need to make it a var as Value Types
-var changedScore = score
+var changedScore = savedScore
 changedScore.score = 200
-guard let savedScore = try? changedScore.save() else { fatalError() }
-assert(score.score == 10)
-assert(score.objectId == changedScore.objectId)
+guard let savedChangedScore = try? changedScore.save() else { fatalError() }
+assert(savedChangedScore.score == 200)
+assert(savedChangedScore.objectId == changedScore.objectId)
 
 let score2 = GameScore(score: 3)
 guard let results = try? GameScore.saveAll(score, score2) else { fatalError() }
