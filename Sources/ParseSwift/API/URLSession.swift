94a9f55c238958de7069383a871fd5f2abd8386c
Corey's iMac <coreyearleon@icloud.com>
2020-07-25 08:29:54 -0400

    Update file comments

--- Sources/ParseSwift/API/URLSession.swift
+++ Sources/ParseSwift/API/URLSession.swift
@@ -2,8 +2,8 @@
 //  URLSession.swift
 //  ParseSwift
 //
-//  Created by Corey Baker on 7/22/20.
-//  Copyright © 2020 Parse. All rights reserved.
+//  Created by Florent Vilmart on 17-09-24.
+//  Copyright © 2017 Parse. All rights reserved.
 //
 
 import Foundation
