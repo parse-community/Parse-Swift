99190aa127176f11ce016c86c7871ee39495a292
Corey MacBook Pro <coreyearleon@icloud.com>
2020-07-30 10:01:12 -0400

    Add watchOS and tvOS builds to travis along with codecov

---
+++ ParseSwift-tvOS/ParseSwift_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  ParseSwift_tvOS.h
+//  ParseSwift-tvOS
+//
+//  Created by Corey Baker on 7/30/20.
+//  Copyright © 2020 Parse Community. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ParseSwift_tvOS.
+FOUNDATION_EXPORT double ParseSwift_tvOSVersionNumber;
+
+//! Project version string for ParseSwift_tvOS.
+FOUNDATION_EXPORT const unsigned char ParseSwift_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParseSwift_tvOS/PublicHeader.h>
+
+
---
+++ ParseSwift-watchOS/ParseSwift_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  ParseSwift_watchOS.h
+//  ParseSwift-watchOS
+//
+//  Created by Corey Baker on 7/30/20.
+//  Copyright © 2020 Parse Community. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ParseSwift_watchOS.
+FOUNDATION_EXPORT double ParseSwift_watchOSVersionNumber;
+
+//! Project version string for ParseSwift_watchOS.
+FOUNDATION_EXPORT const unsigned char ParseSwift_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParseSwift_watchOS/PublicHeader.h>
+
+
