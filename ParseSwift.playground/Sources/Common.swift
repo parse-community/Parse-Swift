b0b130dc2f4aefa7aef6d2ce9e749013d62aec17
cbaker6 <cbaker6@users.noreply.github.com>
2020-08-02 15:36:48 +0000

    deploy: 4d5b33118e5b271cf0d3fdacb5e2d9c7d3f9cd1a

--- ParseSwift.playground/Sources/Common.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import ParseSwift
-
-public func initializeParse() {
-    ParseSwift.initialize(applicationId: "applicationId",
-                     clientKey: "clientKey",
-                     masterKey: "masterKey",
-                     serverURL: URL(string: "http://localhost:1337/1")!)
-}
--- Tests/LinuxMain.swift
+++
@@ -1,5 +0,0 @@
-import XCTest
-
-import ParseSwiftTests
-
-fatalError("Running tests like this is unsupported. Run the tests again by using `swift test --enable-test-discovery`")
