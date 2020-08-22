a8c43acb249333ef971062d94eb442256e2c3afb
Pranjal Satija <me@pranj.co>
2020-08-21 22:33:42 -0500

    add availability check

--- Tests/ParseSwiftTests/ParseEncoderTests.swift
+++ Tests/ParseSwiftTests/ParseEncoderTests.swift
@@ -9,6 +9,9 @@
 import XCTest
 @testable import ParseSwift
 
+// This is necessary because of the .sortedKeys output formatting given to the JSONEncoder
+// That API is only available on macOS 10.13 and up.
+@available(macOS 13, *)
 class ParseEncoderTests: XCTestCase {
     struct Address: Codable {
         let street: String
