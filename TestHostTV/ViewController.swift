b01f4bfc8b13254caddf049ffb6cfea1c708cf1b
Corey's iMac <coreyearleon@icloud.com>
2020-11-08 10:55:55 -0500

    fix tvOS tests by adding TestHost

---
+++ TestHostTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHostTV
+//
+//  Created by Corey Baker on 11/8/20.
+//  Copyright © 2020 Parse Community. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
