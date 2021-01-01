3d81f216900d2bcdce37ee73ed3c3d146acbf31a
Corey's iMac <coreyearleon@icloud.com>
2021-01-01 10:22:54 -0500

    wip

---
+++ Sources/ParseSwift/LiveQuery/ParseLiveQuery.swift
@@ -0,0 +1,16 @@
+//
+//  ParseLiveQuery.swift
+//  ParseSwift
+//
+//  Created by Corey Baker on 12/31/20.
+//  Copyright © 2020 Parse Community. All rights reserved.
+//
+
+import Foundation
+import Network
+
+@available(macOS 10.14, iOS 12.0, watchOS 5.0, tvOS 12.0, *)
+struct ParseLiveQuery {
+    init() {
+    }
+}
