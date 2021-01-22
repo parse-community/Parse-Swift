5736f930ede534e9661e2d82d2aa76ac4c62152c
Corey's iMac <coreyearleon@icloud.com>
2021-01-21 21:21:50 -0500

    Swift Linux missing URLSessionWebSocketTask so it can't use LiveQuery for now

--- Sources/ParseSwift/LiveQuery/LiveQuerySocket.swift
+++ Sources/ParseSwift/LiveQuery/LiveQuerySocket.swift
@@ -5,7 +5,7 @@
 //  Created by Corey Baker on 12/31/20.
 //  Copyright © 2020 Parse Community. All rights reserved.
 //
-
+#if !os(Linux)
 import Foundation
 #if canImport(FoundationNetworking)
 import FoundationNetworking
@@ -148,3 +148,4 @@ extension LiveQuerySocket: URLSessionWebSocketDelegate {
     }
     #endif
 }
+#endif
