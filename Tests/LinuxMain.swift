6fa714be36d04e290242c3094913adf9410e94a3
Corey's iMac <coreyearleon@icloud.com>
2021-02-02 18:27:05 -0500

    generate linux test files

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,4 +2,7 @@ import XCTest
 
 import ParseSwiftTests
 
-fatalError("Running tests like this is unsupported. Run the tests again by using `swift test --enable-test-discovery`")
+var tests = [XCTestCaseEntry]()
+tests += ParseSwiftTests.__allTests()
+
+XCTMain(tests)
