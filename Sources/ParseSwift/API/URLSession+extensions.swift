c241c382df60e24aeb8c7a33b51b96cb8c17f6e4
Corey's iMac <coreyearleon@icloud.com>
2021-03-14 10:48:12 -0400

    Improve decode error

--- Sources/ParseSwift/API/URLSession+extensions.swift
+++ Sources/ParseSwift/API/URLSession+extensions.swift
@@ -43,9 +43,16 @@ extension URLSession {
                     return .failure(error)
                 }
                 guard let parseError = error as? ParseError else {
+                    if let json = try? JSONSerialization
+                        .data(withJSONObject: responseData,
+                              options: .prettyPrinted) {
+                        return .failure(ParseError(code: .unknownError,
+                                                   // swiftlint:disable:next line_length
+                                                   message: "Error decoding parse-server response: \(String(describing: urlResponse)) with error: \(error.localizedDescription) Format: \(String(describing: String(data: json, encoding: .utf8)))"))
+                    }
                     return .failure(ParseError(code: .unknownError,
                                                // swiftlint:disable:next line_length
-                                               message: "Error decoding parse-server response: \(String(describing: urlResponse)) with error: \(error.localizedDescription). Format: \(String(describing: String(data: responseData, encoding: .utf8)))"))
+                                               message: "Error decoding parse-server response: \(String(describing: urlResponse)) with error: \(error.localizedDescription) Format: \(String(describing: String(data: responseData, encoding: .utf8)))"))
                 }
                 return .failure(parseError)
             }
