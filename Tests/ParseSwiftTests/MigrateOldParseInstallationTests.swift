f5d3f060227bfdbc3e0c084e2bdf64234c527525
Corey's iMac <coreyearleon@icloud.com>
2021-04-03 15:18:18 -0400

    Migrate old installations automatically

---
+++ Tests/ParseSwiftTests/MigrateOldParseInstallationTests.swift
@@ -0,0 +1,108 @@
+//
+//  MigrateOldParseInstallationTests.swift
+//  ParseSwift
+//
+//  Created by Corey Baker on 4/3/21.
+//  Copyright © 2021 Parse Community. All rights reserved.
+//
+
+import XCTest
+@testable import ParseSwift
+
+class MigrateOldParseInstallationTests: XCTestCase {
+
+    struct Installation: ParseInstallation {
+        var installationId: String?
+        var deviceType: String?
+        var deviceToken: String?
+        var badge: Int?
+        var timeZone: String?
+        var channels: [String]?
+        var appName: String?
+        var appIdentifier: String?
+        var appVersion: String?
+        var parseVersion: String?
+        var localeIdentifier: String?
+        var objectId: String?
+        var createdAt: Date?
+        var updatedAt: Date?
+        var ACL: ParseACL?
+        var customKey: String?
+    }
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        MockURLProtocol.removeAll()
+        #if !os(Linux) && !os(Android)
+        try KeychainStore.shared.deleteAll()
+        #endif
+        try ParseStorage.shared.deleteAll()
+    }
+
+    func testDontOverwriteMigratedInstallation() throws {
+        guard let url = URL(string: "http://localhost:1337/1") else {
+            XCTFail("Should create valid URL")
+            return
+        }
+        let memory = InMemoryKeyValueStore()
+        ParseStorage.shared.use(memory)
+        var newInstallation = Installation()
+        newInstallation.updateAutomaticInfo()
+        newInstallation.objectId = "yarr"
+        newInstallation.installationId = UUID().uuidString.lowercased()
+        Installation.currentInstallationContainer.installationId = newInstallation.installationId
+        Installation.currentInstallationContainer.currentInstallation = newInstallation
+        Installation.saveCurrentContainerToKeychain()
+
+        ParseSwift.initialize(applicationId: "applicationId",
+                              clientKey: "clientKey",
+                              masterKey: "masterKey",
+                              serverURL: url,
+                              primitiveObjectStore: memory,
+                              testing: true)
+        guard let installation = Installation.current else {
+            XCTFail("Should have installation")
+            return
+        }
+        XCTAssertTrue(installation.hasSameObjectId(as: newInstallation))
+        XCTAssertTrue(installation.hasSameInstallationId(as: newInstallation))
+    }
+
+    func testOverwriteOldInstallation() throws {
+        guard let url = URL(string: "http://localhost:1337/1") else {
+            XCTFail("Should create valid URL")
+            return
+        }
+        let memory = InMemoryKeyValueStore()
+        ParseStorage.shared.use(memory)
+        var newInstallation = Installation()
+        newInstallation.updateAutomaticInfo()
+        newInstallation.installationId = UUID().uuidString.lowercased()
+        Installation.currentInstallationContainer.installationId = newInstallation.installationId
+        Installation.currentInstallationContainer.currentInstallation = newInstallation
+        Installation.saveCurrentContainerToKeychain()
+
+        XCTAssertNil(newInstallation.objectId)
+        guard let oldInstallation = Installation.current else {
+            XCTFail("Should have installation")
+            return
+        }
+        XCTAssertTrue(oldInstallation.hasSameInstallationId(as: newInstallation))
+
+        ParseSwift.initialize(applicationId: "applicationId",
+                              clientKey: "clientKey",
+                              masterKey: "masterKey",
+                              serverURL: url,
+                              primitiveObjectStore: memory,
+                              testing: true)
+        guard let installation = Installation.current else {
+            XCTFail("Should have installation")
+            return
+        }
+        XCTAssertFalse(installation.hasSameInstallationId(as: newInstallation))
+    }
+}
