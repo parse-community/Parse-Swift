9c9b394d2b2c4dd19ba668edf70ef221a9bf8598
Corey's iMac <coreyearleon@icloud.com>
2021-05-15 13:42:56 -0400

    add Kotlin Android files

---
+++ android/app/src/main/java/com/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import android.os.Bundle
+import com.parse.ParseSwift
+
+class MainActivity {
+    companion object {
+        init {
+            System.loadLibrary("DemoApp")
+        }
+    }
+}
