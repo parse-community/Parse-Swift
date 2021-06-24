385ef3f36b718cdf37d6846c674550a05b3c773b
Corey <coreyearleon@icloud.com>
2021-06-24 18:55:15 -0400

    Add some publishers for LiveQuery

---
+++ Sources/ParseSwift/LiveQuery/ParseLiveQuery+combine.swift
@@ -0,0 +1,51 @@
+//
+//  ParseLiveQuery+combine.swift
+//  ParseSwift
+//
+//  Created by Corey Baker on 6/24/21.
+//  Copyright © 2021 Parse Community. All rights reserved.
+//
+
+#if canImport(Combine)
+import Foundation
+import Combine
+
+@available(macOS 10.15, iOS 13.0, macCatalyst 13.0, watchOS 6.0, tvOS 13.0, *)
+extension ParseLiveQuery {
+    // MARK: Functions - Combine
+
+    /**
+     Manually establish a connection to the `ParseLiveQuery` Server.. Publishes when established.
+      - parameter isUserWantsToConnect: Specifies if the user is calling this function. Defaults to `true`.
+      - returns: A publisher that eventually produces a single value and then finishes or fails.
+    */
+    public func openPublisher(isUserWantsToConnect: Bool = true) -> Future<Void, Error> {
+        Future { promise in
+            self.open(isUserWantsToConnect: isUserWantsToConnect) { error in
+                guard let error = error else {
+                    promise(.success(()))
+                    return
+                }
+                promise(.failure(error))
+            }
+        }
+    }
+
+    /**
+     Sends a ping frame from the client side. Publishes when a pong is received from the
+     server endpoint.
+     - returns: A publisher that eventually produces a single value and then finishes or fails.
+    */
+    public func sendPingPublisher() -> Future<Void, Error> {
+        Future { promise in
+            self.sendPing { error in
+                guard let error = error else {
+                    promise(.success(()))
+                    return
+                }
+                promise(.failure(error))
+            }
+        }
+    }
+}
+#endif
