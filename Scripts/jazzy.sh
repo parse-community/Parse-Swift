0d9257612283e3fbb6c69ce7a0de3a9b09a6716d
Corey's iMac <coreyearleon@icloud.com>
2021-06-26 20:39:46 -0400

    setup autoversioning WIP

--- Scripts/jazzy.sh
+++ Scripts/jazzy.sh
@@ -1,11 +1,11 @@
-ver=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ParseSwift-iOS/Info.plist`
+ver=`cat ParseSwift.xcodeproj/project.pbxproj | grep -m1 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' '`
 bundle exec jazzy \
   --clean \
   --author "Parse Community" \
   --author_url http://parseplatform.org \
   --github_url https://github.com/parse-community/Parse-Swift \
   --root-url http://parseplatform.org/Parse-Swift/api/ \
-  --module-version 1.8.4 \
+  --module-version ${ver} \
   --theme fullwidth \
   --skip-undocumented \
   --output ./docs/api \
