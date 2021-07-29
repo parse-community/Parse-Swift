e60d8e31971cfcba7d0fff47db806642d17ee41c
dblythy <daniel-blyth@live.com.au>
2021-07-29 22:16:25 +1000

    fix tests

--- Tests/ParseSwiftTests/ParseFacebookCombineTests.swift
+++ Tests/ParseSwiftTests/ParseFacebookCombineTests.swift
@@ -61,22 +61,6 @@ class ParseFacebookCombineTests: XCTestCase { // swiftlint:disable:this type_bod
             self.username = "hello10"
             self.email = "hello@parse.com"
         }
-
-        func testMakeDictionary() {
-            let authData = ParseFacebook<User>
-            .AuthenticationKeys.id.makeDictionary(userId: "testing",
-                                                  accessToken: "testAccessToken",
-                                                  authenticationToken: "testAuthToken")
-            XCTAssertEqual(authData["id"],"testing")
-            XCTAssertEqual(authData["access_token"],"testAccessToken")
-
-            let secondAuthData = ParseFacebook<User>
-            .AuthenticationKeys.id.makeDictionary(userId: "testing",
-                                                  accessToken: nil,
-                                                  authenticationToken: "testAuthToken")
-            XCTAssertEqual(secondAuthData["id"],"testing")
-            XCTAssertEqual(secondAuthData["token"],"testAuthToken")
-        }
     }
 
     override func setUpWithError() throws {
