b02a33998cb351f4480223207139949679ca03aa
Corey <coreyearleon@icloud.com>
2021-09-24 00:37:38 -0400

    Don't run async tests on Android

--- Tests/ParseSwiftTests/ParseObjectAsyncTests.swift
+++ Tests/ParseSwiftTests/ParseObjectAsyncTests.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2021 Parse Community. All rights reserved.
 //
 
-#if swift(>=5.5) && canImport(_Concurrency)
+#if swift(>=5.5) && canImport(_Concurrency) && !os(Linux) && !os(Android)
 import Foundation
 import XCTest
 @testable import ParseSwift
