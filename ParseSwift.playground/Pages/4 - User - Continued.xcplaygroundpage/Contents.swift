7932ba7d76d4a4f3bdc3e5e8698c5131f13d53a5
Corey <coreyearleon@icloud.com>
2021-10-07 12:07:23 -0400

    Improve API documentation (#253)
    * Improve API documentation
    
    * Add SwiftUI info to API docs

--- ParseSwift.playground/Pages/4 - User - Continued.xcplaygroundpage/Contents.swift
+++ ParseSwift.playground/Pages/4 - User - Continued.xcplaygroundpage/Contents.swift
@@ -36,8 +36,8 @@ struct User: ParseUser {
     /*:
      It's recommended the developer adds the emptyObject computed property or similar.
      Gets an empty version of the respective object. This can be used when you only need to update a
-     a subset of the fields of an object as oppose to updating every field of an object.
-     - note: Using an empty object and updating a subset of the fields reduces the amount of data sent between
+     a subset of the fields of an object as oppose to updating every field of an object. Using an
+     empty object and updating a subset of the fields reduces the amount of data sent between
      client and server when using `save` and `saveAll` to update objects.
     */
     var emptyObject: Self {
