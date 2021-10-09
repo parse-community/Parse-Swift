a85aa72873788abc21a6e9ea1335b97e577566e8
dblythy <daniel-blyth@live.com.au>
2021-10-09 17:43:21 +1100

    Update ParseQueryTests.swift

--- Tests/ParseSwiftTests/ParseQueryTests.swift
+++ Tests/ParseSwiftTests/ParseQueryTests.swift
@@ -705,7 +705,6 @@ class ParseQueryTests: XCTestCase { // swiftlint:disable:this type_body_length
         }
     }
 
-    #if !os(Linux) && !os(Android)
     func testFirstThrowDecodingError() {
         var scoreOnServer = GameScoreBroken()
         scoreOnServer.objectId = "yarr"
@@ -732,9 +731,11 @@ class ParseQueryTests: XCTestCase { // swiftlint:disable:this type_body_length
                 XCTFail("Should have casted as ParseError")
                 return
             }
+            #if !os(Linux) && !os(Android)
             // swiftlint:disable:next line_length
             XCTAssertEqual(error.message, "Invalid struct: No value associated with key CodingKeys(stringValue: \"score\", intValue: nil) (\"score\").")
             XCTAssertEqual(error.code, .unknownError)
+            #endif
         }
         XCTAssertThrowsError(try query.first(options: []))
     }
