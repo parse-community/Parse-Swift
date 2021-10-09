68c22116869e2a41b3c120fa5bdacdfe82a61653
dblythy <daniel-blyth@live.com.au>
2021-10-09 17:04:03 +1100

    Update ParseQueryTests.swift

--- Tests/ParseSwiftTests/ParseQueryTests.swift
+++ Tests/ParseSwiftTests/ParseQueryTests.swift
@@ -731,6 +731,7 @@ class ParseQueryTests: XCTestCase { // swiftlint:disable:this type_body_length
                 XCTFail("Should have casted as ParseError")
                 return
             }
+            #if !os(Linux) && !os(Android)
             // swiftlint:disable:next line_length
             XCTAssertEqual(error.message, "Invalid struct: No value associated with key CodingKeys(stringValue: \"score\", intValue: nil) (\"score\").")
             XCTAssertEqual(error.code, .unknownError)
