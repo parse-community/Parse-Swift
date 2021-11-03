9d2680bf86a39fa2ef1b9dcf3cf35c727bfb0662
Corey Baker <coreyearleon@icloud.com>
2021-11-03 13:28:52 -0400

    WIP

---
+++ Sources/ParseSwift/LiveQuery/SubscriptionPublisher.swift
@@ -0,0 +1,14 @@
+//
+//  SubscriptionPublisher.swift
+//  ParseSwift
+//
+//  Created by Corey Baker on 11/3/21.
+//  Copyright © 2021 Parse Community. All rights reserved.
+//
+
+
+#if canImport(Combine)
+import Foundation
+import Combine
+
+#endif
