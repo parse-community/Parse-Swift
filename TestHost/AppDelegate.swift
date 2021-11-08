8ce890502a02180e5db0120fe4767e1873068331
Corey Baker <coreyearleon@icloud.com>
2021-11-08 09:04:33 -0500

    nits

--- TestHost/AppDelegate.swift
+++ TestHost/AppDelegate.swift
@@ -15,12 +15,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-}
