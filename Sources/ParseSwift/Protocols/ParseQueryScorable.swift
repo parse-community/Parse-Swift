e12b88f13fe02c0a46bc97fc235d8220f8c3841a
Corey Baker <coreyearleon@icloud.com>
2022-01-17 18:07:48 -0500

    Move the where for Swift 5.2

--- Sources/ParseSwift/Protocols/ParseQueryScorable.swift
+++ Sources/ParseSwift/Protocols/ParseQueryScorable.swift
@@ -22,14 +22,14 @@ public protocol ParseQueryScorable {
 }
 
 // MARK: ParseQueryScorable
-extension Query {
+extension Query where T: ParseObject & ParseQueryScorable {
     /**
       Method to sort the full text search by text score.
       - parameter value: String or Object of index that should be used when executing query.
       - note: Your `ParseObject` should conform to `ParseQueryScorable` to retrieve
       the weight/rank via  the "score" property of your `ParseObject`.
     */
-    public func sortByTextScore() -> Query<T> where T: ParseObject & ParseQueryScorable {
+    public func sortByTextScore() -> Query<T> {
         var mutableQuery = self
         let ascendingScore = Order.ascending(QueryConstraint.Comparator.score.rawValue)
         if mutableQuery.order != nil {
