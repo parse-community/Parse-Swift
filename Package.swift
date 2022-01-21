2132eb0c583e847ca234f08e6eac49923a307d1a
Corey Baker <coreyearleon@icloud.com>
2022-01-21 11:30:58 -0500

    fix warning

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "ParseSwiftTests",
-            dependencies: ["ParseSwift"])
+            dependencies: ["ParseSwift"],
+            exclude: ["Info.plist"])
     ]
 )
