aa02ed9982b5dc9efd787263b23395fa8e581d2c
Corey <coreyearleon@icloud.com>
2022-01-29 14:57:23 -0500

    fix: add 5.1 SPM file (#337)

---
+++ Package@5.1.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "ParseSwift",
+    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+    products: [
+        .library(
+            name: "ParseSwift",
+            targets: ["ParseSwift"])
+    ],
+    targets: [
+        .target(
+            name: "ParseSwift",
+            dependencies: []),
+        .testTarget(
+            name: "ParseSwiftTests",
+            dependencies: ["ParseSwift"],
+            exclude: ["Info.plist"])
+    ]
+)
